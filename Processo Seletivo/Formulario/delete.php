0e47a5903b16597f2b57f74c0245c57a3e3ce543
ItaloRafaelSilva <italorafaelsilva20@gmail.com>
2023-02-04 13:43:40 -0300

    Processo

---
+++ Processo Seletivo/Formulario/delete.php
@@ -0,0 +1,25 @@
+<?php
+
+if(!empty($_GET['id']))
+{
+    include_once('config.php');
+
+    $id = $_GET['id'];
+
+    $sqlSelect = "SELECT *  FROM usuarios WHERE id=$id";
+
+    $result = $conexao->query($sqlSelect);
+
+    if($result->num_rows > 0)
+    {
+        $sqlDelete = "DELETE FROM usuarios WHERE id=$id";
+        $resultDelete = $conexao->query($sqlDelete);
+    }
+}
+if (mysqli_query($conexao,$sqlDelete)){
+    echo "<script>alert('Cadastro deletador com sucesso!');window.location = 'sistema.php';</script>";
+}else{
+    echo 'Deu Erro';
+}
+
+?>
